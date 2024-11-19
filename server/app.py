efae2b3f19eb33f4450caa6eb96ac5e883a0c02d
Matteo Piccini <gnappo.1@live.it>
2024-11-19 15:18:18 -0800

    end of walkthrough

--- server/app.py
+++ server/app.py
@@ -6,6 +6,8 @@ from models import db
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+
 
 migrate = Migrate(app, db)
 
